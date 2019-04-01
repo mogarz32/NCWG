ec3a7fbf2615fce470dd29edb09ef44ff1f7763e
Zach Ressler <sockoman32@aol.com>
2019-04-01 19:19:04 -0400

    Updates formatting, adds tabs to the new medical record page, and tweaks javascript.

--- scripts/index.js
+++ scripts/index.js
@@ -15,8 +15,23 @@ function filterTable(event) {
     }
 }
 
-function navigate() {
-	window.location.href = "patients/00002.html";
+function openTab(event, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
 }
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 
 document.querySelector('#search').addEventListener('keyup', filterTable, false);
